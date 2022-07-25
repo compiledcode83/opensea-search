d6cb4484a548dab72968b735a7600aabf91d3dac
residentevil0803 <4081040+residentevil0803@users.noreply.github.com>
2022-07-25 14:08:09 +0300

    update connect wallet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
+import { ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import theme from '../styles/theme/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
