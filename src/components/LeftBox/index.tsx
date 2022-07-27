3a1d6578a8dd83a2d601128ef7e8ce93c6380b55
residentevil0803 <4081040+residentevil0803@users.noreply.github.com>
2022-07-27 13:20:12 +0300

    update code

--- src/components/LeftBox/index.tsx
+++ src/components/LeftBox/index.tsx
@@ -148,7 +148,7 @@ const LeftBox: React.FC = () => {
   const [assetContractAddress, setAssetContractAddress] = useState<string>('')
   const { provider, web3Provider, address, chainId } = state
 
-  const web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546')
+  const web3 = new Web3(Web3.givenProvider)
 
   const connect = useCallback(async function () {
     // This is the initial `provider` that is returned when
